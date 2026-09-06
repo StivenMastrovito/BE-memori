076888f2add883259f28df42218ae2d33a8d245c
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-09-06 19:02:09 +0200

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://fe-memori.vercel.app', 'http://localhost:5173'],
+    'allowed_origins' => ['https://fe-memori.vercel.app', 'http://localhost:5173', 'https://me.mori.online'],
 
     'allowed_origins_patterns' => [],
 
